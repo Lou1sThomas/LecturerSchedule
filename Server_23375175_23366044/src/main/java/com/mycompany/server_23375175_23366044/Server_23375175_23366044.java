faf8ac385be71f493a8b09a78f7a1aacf5071706
ocounihan <orancounihan@gmail.com>
2025-02-24 19:25:34 +0000

    First basic version of the server application

--- Server_23375175_23366044/src/main/java/com/mycompany/server_23375175_23366044/Server_23375175_23366044.java
+++ Server_23375175_23366044/src/main/java/com/mycompany/server_23375175_23366044/Server_23375175_23366044.java
@@ -6,11 +6,56 @@ package com.mycompany.server_23375175_23366044;
 
 /**
  *
- * @author louis
+ * @author oranc
  */
-public class Server_23375175_23366044 {
+import java.io.*;
+import java.net.*;
+public class Server_23375175_23366044{
 
+    private static ServerSocket servSock;
+    private static final int PORT = 6754;
+    private static int clientConnections = 0;
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Opening Port.../n");
+        try{
+            servSock = new ServerSocket(PORT);
+        }
+        catch (IOException e){
+            System.out.println("Unable to attach to port.");
+            System.exit(1);
+        }
+        do {
+            run();
+        } while (true);
     }
+    
+    private static void run() {
+        Socket link = null;
+        
+        try{
+            link = servSock.accept();
+            clientConnections++;
+            
+            BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream()));
+            PrintWriter out = new PrintWriter(link.getOutputStream(),true);
+            String message = in.readLine();
+            System.out.println("Message recieved from client: " + clientConnections + " " + message);
+            out.println("Echo message " + message);
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try{
+                System.out.println("/n Closing Connection");
+                link.close();
+                
+            } catch(IOException e) {
+                System.out.println("Unable to disconnect");
+                System.exit(1);
+            }
+        }
+    }
+    
 }
