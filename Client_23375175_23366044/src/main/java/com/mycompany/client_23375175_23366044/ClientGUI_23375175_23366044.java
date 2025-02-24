40c3b1213a38c42f9fc57eb2d845ed756ec05e6c
ocounihan <orancounihan@gmail.com>
2025-02-24 19:25:15 +0000

    First basic version of the client application

--- Client_23375175_23366044/src/main/java/com/mycompany/client_23375175_23366044/ClientGUI_23375175_23366044.java
+++ Client_23375175_23366044/src/main/java/com/mycompany/client_23375175_23366044/ClientGUI_23375175_23366044.java
@@ -1,30 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.mycompany.client_23375175_23366044;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
- * JavaFX App
+ *
+ * @author oranc
  */
-public class ClientGUI_23375175_23366044 extends Application {
-
-    @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+public class ClientGUI_23375175_23366044 {
+    private static final int PORT = 6754;
+    private static InetAddress host;
+    
+    public static void main(String[] args){
+        try{
+            host = InetAddress.getLocalHost();
+    }
+        catch (UnknownHostException e) {
+            System.out.println("Host ID not found.");
+            System.exit(1);
+        }
+        run();
     }
 
-    public static void main(String[] args) {
-        launch();
+    
+    private static void run(){
+        Socket link = null;
+        try {
+            link = new Socket(host, PORT);
+            BufferedReader input = new BufferedReader(new InputStreamReader(link.getInputStream()));
+            PrintWriter out = new PrintWriter(link.getOutputStream(),true);
+            BufferedReader userEntry = new BufferedReader(new InputStreamReader(System.in));
+            String message = null; String response = null;
+            
+            System.out.println("Enter message:");
+            message = userEntry.readLine();
+            out.print(message);
+            response = input.readLine();
+            System.out.println("\nServer response: " + response);
+          
+        }
+            catch(IOException e) {
+                e.printStackTrace();
+            }
+                finally{
+                    try{
+                        System.out.println("Closing Connection...");
+                        link.close();
+                    }
+                        catch (IOException e) {
+                            System.out.println("Unable to disconnect.");
+                            System.exit(1);
+                        }
+        }
     }
-
 }
+        
+
